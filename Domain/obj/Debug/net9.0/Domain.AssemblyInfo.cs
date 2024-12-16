9d5c38135aa2a051dfebfecea26ce942c47d6cff
100T Christian <chrisdev@100t>
2024-12-15 21:57:18 -0500

    Dependencias para Persistence added

--- Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net9.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6ceb09e33f02583191e0903f6095e4eed286371")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f30e562e56696a2de422aa14da6d1455da4337e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
